6053bfdeb9b3ec02610a58037da3174863f8eb4e
Ian <fakeemail@commit.com>
2017-04-03 19:51:14 -0400

    alters unh698.py

--- unh698.py
+++ unh698.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Dear python Gods, please be merciful'
+    return 'Dear python Gods, please be merciful. UNH698 Website'
 
 if __name__ == '__main__':
     unittest
